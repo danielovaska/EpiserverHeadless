48e527d13a82375b363a90a7d7c755dbe27c940e
Daniel Ovaska <daniel.ovaska@hotmail.com>
2018-06-15 17:18:35 +0200

    Adding angular6

--- EpiserverAngular/Business/Initialization/BundleConfig.cs
+++ EpiserverAngular/Business/Initialization/BundleConfig.cs
@@ -22,14 +22,22 @@ namespace EpiserverAngular.Business.Initialization
                         "~/Static/js/jquery.js", //jquery.js can be removed and linked from CDN instead, we use a local one for demo purposes without internet connectionzz
                         "~/Static/js/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular/js").Include(
+                      
+                        "~/my-pages-app/dist/my-pages-app/runtime.*",
+                        "~/my-pages-app/dist/my-pages-app/polyfills.*",
+                        "~/my-pages-app/dist/my-pages-app/styles.*",
+                        "~/my-pages-app/dist/my-pages-app/vendor.*",
+                        "~/my-pages-app/dist/my-pages-app/main.*"));
+
             bundles.Add(new StyleBundle("~/bundles/css")
                 .Include("~/Static/css/bootstrap.css", new CssRewriteUrlTransform())
                 .Include("~/Static/css/bootstrap-responsive.css")
                 .Include("~/Static/css/media.css")
                 .Include("~/Static/css/style.css", new CssRewriteUrlTransform())
                 .Include("~/Static/css/editmode.css"));
+            
         }
-
         public void Uninitialize(InitializationEngine context)
         {
         }
