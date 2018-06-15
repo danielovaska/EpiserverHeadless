2140b3317a79a2ac0c316c250b0361a9bae52085
Daniel Ovaska <daniel.ovaska@hotmail.com>
2018-06-15 15:07:59 +0200

    Add project files.

---
+++ EpiserverAngular/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace EpiserverAngular.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
